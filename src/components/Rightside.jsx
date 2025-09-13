b2415a247683edf4956ceebb3514e26c9184c415
Alagham <hamzahalagbe27@gmail.com>
2025-09-13 21:52:18 +0100

    hero function settled

--- src/components/Rightside.jsx
+++ src/components/Rightside.jsx
@@ -40,6 +40,7 @@ const Rightside = (props) => {
           src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
           alt=""
         />
+        
       </BannerCard>
     </Container>
   );
